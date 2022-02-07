3afd44e411d9aa00b21fa7573f9ac0f3cb49f5db
Noah2211 <noah.decroix3@gmail.com>
2022-02-07 14:36:58 +0100

    done

---
+++ VODService.php
@@ -0,0 +1,40 @@
+<?php
+class VODService {
+    public string $name;
+    public float $price;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+}
--- index.php
+++ index.php
@@ -6,9 +6,13 @@
  *
  * 2. Créez les getters et les setters associés à ces propriétés.
  *
- * 3. Fites en sorte de pouvoir chaîner le deux setters
+ * 3. Faites en sorte de pouvoir chaîner le deux setters
  *
  * 4. Instancier un objet à l'aide de la classe créée, instanciez dans le fichier index.php
  *
  * 5. Utilisez le chaînage des méthodes pour mettre à jour le nom et le prix en une seule ligne.
  */
+
+$vod = new VODService();
+
+$vod->setName("Mon nom")->setPrice(12);
